eeeabeb072ebb761b95ee7569171cd5a93ed3427
zhanglinwei <zhang_linwei@sina.cn>
2016-08-30 22:00:44 +0800

    第三次提交
    优化 Request。
     1.重构了构造方法
     2.修改不好的命名
    
    待解决问题
     1.请求默认路径
     2.图片刷新问题

--- src/server/Request.java
+++ src/server/Request.java
@@ -7,9 +7,8 @@ import java.io.InputStream;
 public class Request {
 
 	private InputStream in;
-	private DataInputStream input;
 	private String method;
-	private String uri;
+	private String url;
 	private byte[] content;
 
 	/**
@@ -20,40 +19,42 @@ public class Request {
 	 */
 	public Request(InputStream in) throws IOException {
 		this.in = in;
-		this.input = new DataInputStream(in);
-		// 读出第一行内容
-		String msg = input.readLine();
-		// 初始化 method
-		parseMethod(msg);
-		// 初始化 url
-		parseUri(msg);
-		
-		
+		praseRequest();
+	}
+
+	private void praseRequest() throws IOException{
+		DataInputStream input = new DataInputStream(in);
+		// 读去请求行内容
+		@SuppressWarnings("deprecation")
+		String requestLine = input.readLine();
+		parseMethod(requestLine);
+		parseUrl(requestLine);
 		// 如果方法为post，则初始化报文体内容 content
 		if (method.equals("POST")) {
 			// 解析并初始化 content
-			ParserContent();
+			parserContent();
 		}
 	}
-
+	
 	/**
 	 * 解析 content 的内容
 	 * @throws IOException
 	 */
-	private void ParserContent() throws IOException {
-
+	@SuppressWarnings("deprecation")
+	private void parserContent() throws IOException {
+		DataInputStream input = new DataInputStream(in);
 		// 读取第一行
-		String string = input.readLine();
+		String line = input.readLine();
 		// 报文长度
 		int len = 0;
 
-		while (!string.equals("")) {
-			if (string.contains("Content-Length")) {
-				int index = string.indexOf(" ");
-				string = string.substring(index + 1);
-				len = Integer.parseInt(string);
+		while (!line.equals("")) {
+			if (line.contains("Content-Length")) {
+				int index = line.indexOf(" ");
+				line = line.substring(index + 1);
+				len = Integer.parseInt(line);
 			}
-			string = input.readLine();
+			line = input.readLine();
 		}
 		// 创建字节数组接收 content 的内容
 		content = new byte[len];
@@ -66,42 +67,35 @@ public class Request {
 	/**
 	 * 解析资源路径
 	 * 
-	 * @param msg
+	 * @param requestLine
 	 */
-	private void parseUri(String msg) {
+	private void parseUrl(String requestLine) {
 		// 一般的HTTP请求报文的第一行是“GET /index.html HTTP/1.1”
 		// 我们要获取的就是中间的"/indext.apsx"
-
-		// 获取资源的位置
-		uri = msg.substring(msg.indexOf(' ') + 1);
-		uri = uri.substring(1, uri.lastIndexOf(' '));
-
+		url = requestLine.substring(requestLine.indexOf(' ') + 1);
+		url = url.substring(1, url.lastIndexOf(' '));
 		// 默认资源为index.html
-		if (uri.equals(""))
-			uri = "index.html";
+		if (url.equals(""))
+			url = "index.html";
 	}
 
 	/**
 	 * 解析方法名称
 	 * 
-	 * @param msg
+	 * @param reuqestLine
 	 */
-	private void parseMethod(String msg) {
-		// *****
+	private void parseMethod(String reuqestLine) {
 		// 一般的HTTP请求报文的第一行是“GET /index.html HTTP/1.1”
 		// 我们要获取的就是中间的"GET"
-
-		// 获取方法
-		method = msg.substring(0, msg.indexOf(' ') + 1).trim();
+		method = reuqestLine.substring(0, reuqestLine.indexOf(' '));
 	}
 
 	public String getMethod() {
-		// 返回方法
 		return method;
 	}
 
-	public String getUri() {
-		return uri;
+	public String getUrl() {
+		return url;
 	}
 
 	public byte[] getContent() {
--- src/server/Service.java
+++ src/server/Service.java
@@ -1,9 +1,5 @@
 package server;
 
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.PrintStream;
 import java.net.Socket;
 
 //每有一个连接建立时，服务器分出一个通信的线程
@@ -13,47 +9,6 @@ public class Service implements Runnable {
 
 	public Service(Socket s) {
 		client = s;
-	}
-
-	// 获取浏览器请求资源的路径
-	public String getResourcePath(String s) {
-		// 一般的HTTP请求报文的第一行是“GET /index.html HTTP/1.1”
-		// 我们要获取的就是中间的"/indext.apsx"
-
-		// 获取资源的位置
-		String s1 = s.substring(s.indexOf(' ') + 1);
-		s1 = s1.substring(1, s1.indexOf(' '));
-
-		// 默认资源为index.html
-		if (s1.equals("")){
-			s1 = "index.html";			
-		}
-		return s1;
-	}
-
-	// 获取请求方法
-	public String getMethod(String s) {
-		// 一般的HTTP请求报文的第一行是“GET /index.html HTTP/1.1”
-		// 我们要获取的就是中间的"/indext.apsx"
-
-		// 获取资源的位置
-		String s1 = s.substring(0, s.indexOf(' ') + 1);
-		return s1;
-	}
-
-	public void sendFile(PrintStream out, File file) {
-		try {
-			DataInputStream in = new DataInputStream(new FileInputStream(file));
-			int len = (int) file.length();
-			byte buf[] = new byte[len];
-			in.readFully(buf);// 读取文件内容到buf数组中
-			out.write(buf, 0, len);
-			out.flush();
-			in.close();
-		} catch (Exception e) {
-			System.out.println(e.getMessage());
-			System.exit(1);
-		}
 	}
 
 	public void run() {
@@ -67,7 +22,7 @@ public class Service implements Runnable {
 			String method = request.getMethod();
 			System.out.println("The request's method is: " + method);
 			// 获取文件路径
-			String fileName = request.getUri();
+			String fileName = request.getUrl();
 			System.out.println("The user asked for resource: " + fileName);
 			
 			// response 根据 method 和 filename 作响应
